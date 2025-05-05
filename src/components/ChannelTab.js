1d641e0f9cf497ee1fdaa9adfc47d02d057e4487
Derek Radcliffe <dereksradcliffe@gmail.com>
2025-05-05 00:00:33 -0400

    Updated channel tab and sidebar styles

--- src/components/ChannelTab.js
+++ src/components/ChannelTab.js
@@ -1,26 +1,66 @@
-import { FaAngleDown, FaPlus, FaHashtag } from "react-icons/fa";
+import { useState } from "react";
+import { BsHash } from "react-icons/bs";
+import { FaChevronDown, FaChevronRight, FaPlus } from "react-icons/fa";
+
+const topics = ["tailwind-css", "react"];
+const questions = ["jit-compilation", "purge-files", "dark-mode"];
+const random = ["variants", "plugins"];
 
 const ChannelSideBar = () => {
   return (
     <div className="channelSideBar">
-      <TabHeader tabTitle="Testing title" />
+      <ChannelBlock />
       <div className="channelWrapper">
-        <TabChannel title="General" />
+        <Dropdown header="Topics" selections={topics} />
+        <Dropdown header="Questions" selections={questions} />
+        <Dropdown header="Random" selections={random} />
       </div>
     </div>
   );
 };
 
-const TabHeader = ({tabTitle}) => {
-    return(
-        <div className="tabHeader"><FaAngleDown />{tabTitle}<FaPlus className="plus" /></div>  
-    );
+const Dropdown = ({ header, selections }) => {
+  const [expanded, setExpanded] = useState(true);
+
+  return (
+    <div className="dropdown">
+      <div onClick={() => setExpanded(!expanded)} className="dropdownHeader">
+        <ChevronIcon expanded={expanded} />
+        <h5
+          className={
+            expanded ? "dropdown-header-text-selected" : "dropdown-header-text"
+          }
+        >
+          {header}
+        </h5>
+        <FaPlus size="12" className="plus" />
+      </div>
+      {expanded &&
+        selections &&
+        selections.map((selection) => <TopicSelection selection={selection} />)}
+    </div>
+  );
 };
 
-const TabChannel = ({title}) => { 
-    return(
-        <div className="tabChannel"><FaHashtag size="1rem" />{title}</div>  
-    );
+const ChevronIcon = ({ expanded }) => {
+  return expanded ? (
+    <FaChevronDown size="14" className="arrow chevDown" />
+  ) : (
+    <FaChevronRight size="14" className="arrow chevUp" />
+  );
 };
+
+const TopicSelection = ({ selection }) => (
+  <div className="dropdownSelection">
+    <BsHash size="24" />
+    <h5 className="dropdown-selection-text">{selection}</h5>
+  </div>
+);
+
+const ChannelBlock = () => (
+  <div className="channelBlockHeader">
+    <h5>Channels</h5>
+  </div>
+);
 
 export default ChannelSideBar;
